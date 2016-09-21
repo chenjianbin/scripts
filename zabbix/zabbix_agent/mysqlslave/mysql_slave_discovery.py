b53c2155f2275558b9a0e1f2b74abfeecfbe66df
chenjianbin <546391242@qq.com>
2016-09-21 23:10:40 +0800

    add filebeat

--- zabbix/zabbix_agent/mysqlslave/mysql_slave_discovery.py
+++ zabbix/zabbix_agent/mysqlslave/mysql_slave_discovery.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
-from pathlib import Path, PurePath
+from pathlib import Path,PurePath
 import json
-import re
 
 MYSQL_PATH = '/data0/mysql/'
 
 def discover(path):
-	ports = [ {'{#REDISPORT}':PurePath(p).name} for p in Path(path).glob('*') if p.is_dir() and re.match('[1-65535]', PurePath(p).name) ]
-	print(json.dumps({'data':ports},sort_keys=True,indent=4,separators=(',',':')))
-	
+        ports = [ {'{#MYSQLPORT}':PurePath(d).name} for d in Path(path).glob('*') if d.is_dir() and PurePath(d).name not in ('3306', '99999', 'restore') ]
+        print(json.dumps({'data':ports},sort_keys=True,indent=4,separators=(',',':')))
+
 
 discover(MYSQL_PATH)
