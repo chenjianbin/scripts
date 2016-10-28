33e978b20d82966c8ce08285b6882407e2df7391
chenjianbin <546391242@qq.com>
2016-10-28 16:32:52 +0800

    add web_monitor deug log

--- zabbix/zabbix_agent/web/web_monitor.py
+++ zabbix/zabbix_agent/web/web_monitor.py
@@ -9,19 +9,20 @@ import sys
 WEB_PATH = '/data0/web/'
 
 def discover(path):
-	ports = [ {'{#WEBDOMAIN}':PurePath(p).name} for p in Path(path).glob('*') if p.is_dir() and re.match('.*\..*', PurePath(p).name) ]
-	print(json.dumps({'data':ports},sort_keys=True,indent=4,separators=(',',':')))
+    ports = [ {'{#WEBDOMAIN}':PurePath(p).name} for p in Path(path).glob('*') if p.is_dir() and re.match('.*\..*', PurePath(p).name) ]
+    print(json.dumps({'data':ports},sort_keys=True,indent=4,separators=(',',':')))
 
 def status(domain):
-	url = 'http://{0}'.format(domain)
-	req = urllib.request.Request(url, method='HEAD')
-	try:
-		with urllib.request.urlopen(req) as r:
-			print(r.getcode())
-	except urllib.error.URLError as e:
-		print(e.code) if hasattr(e, 'code') else print(555)
-	#	print(e.code) if hasattr(e, 'code') else print(555)
+    url = 'http://{0}'.format(domain)
+    req = urllib.request.Request(url, method='HEAD')
+    try:
+        with urllib.request.urlopen(req) as r:
+            print(r.getcode())
+    except urllib.error.URLError as e:
+        print(e.code) if hasattr(e, 'code') else print(555)
+        #with open('web.log','a') as logfile:
+        #    logfile.write(str(e))
 
 if __name__ == '__main__':
-	discover(WEB_PATH) if len(sys.argv) == 1 else status(sys.argv[1])
+    discover(WEB_PATH) if len(sys.argv) == 1 else status(sys.argv[1])
 	
