c59b223cbdf30a8116140a2dfdbd5ef70f838509
Your Name <you@example.com>
2022-09-21 16:59:45 +0800

    支持slack

--- mysql/backup/default.backup.py
+++ mysql/backup/default.backup.py
@@ -8,66 +8,81 @@ import time
 import smtplib
 import shlex
 import datetime
+import urllib.request
+import urllib.parse
+import socket
 
 MYSQL_DIR = '/data0/mysql'
 ''' Backup_DataDir 千万别在根目录，否则会很悲催。'''
-BACKUP_DATADIR = '/data0/backups/mysql'
+BACKUP_DATADIR = '/data1/backups/mysql'
 CLEAN_DAY = 7
-MAIL_MSG = ''
+MSG = ''
 
 def backup():
-	global MAIL_MSG
-	MAIL_MSG = MAIL_MSG + '<h1>数据库备份</h1>'
-	ports = [ p.name for p in Path(MYSQL_DIR).glob('*') if p.is_dir() and re.fullmatch('[1-5]?[0-9]{1,4}', p.name) ]
-	result_msg = {}
-	for port in ports:
-		mysql_defaults_file = Path(MYSQL_DIR) / port / 'my.cnf'	
-		backup_dir = Path(BACKUP_DATADIR) / port
-		if mysql_defaults_file.is_file():
-			if not backup_dir.exists():
-				backup_dir.mkdir(parents=True)
-			gz_file = str(backup_dir / (datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + '.tar.gz'))
-			cmd1 = shlex.split('innobackupex --defaults-file=/data0/mysql/%s/my.cnf --user=root --socket=/data0/mysql/%s/mysql.sock  --stream=tar /tmp/' % (port, port))
-			with open(gz_file, 'wb') as f:
-				child1 = Popen(cmd1, stdout=PIPE)
-				child2 = Popen('gzip', stdin=child1.stdout, stdout=f)
-				child2.wait()
-				child1.wait()
-				if child1.returncode or child2.returncode:
-					MAIL_MSG = MAIL_MSG + '<p>%s : Backup Failed!</p>' % port
-				else:
-					MAIL_MSG = MAIL_MSG + '<p>%s : Backup Success!</p>' % port
-		else:
-			MAIL_MSG = MAIL_MSG + '<p>%s : Not Found my.cnf!</p>' % port
-		
+    global MSG
+    MSG = MSG + 'Host: ' + socket.gethostname() + ' Databases Backup\n'
+    ports = sorted([ p.name for p in Path(MYSQL_DIR).glob('*') if p.is_dir() and re.fullmatch('[2-5]?[0-9]{1,4}', p.name) ])
+    result_msg = {}
+    for port in ports:
+        mysql_defaults_file = Path('/etc/my.cnf.d') / 'mysqld@{}.cnf'.format(port)
+        backup_dir = Path(BACKUP_DATADIR) / port
+        if mysql_defaults_file.is_file():
+            if not backup_dir.exists():
+                backup_dir.mkdir(parents=True)
+            backup_file = str(backup_dir / (datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + '.xbstream'))
+            cmd1 = shlex.split('xtrabackup --defaults-file=/etc/my.cnf.d/mysqld@{0}.cnf --backup --user=root --socket=/data0/mysql/{0}/mysql.sock  --stream=xbstream --compress --target-dir=/tmp'.format(port))
+            with open(backup_file, 'wb') as f:
+                child = Popen(cmd1, stdout=f)
+                child.wait()
+                if child.returncode:
+                    MSG = MSG + port + ' : Backup Failed!\n'
+                else:
+                    MSG = MSG + port + ' : Backup Success!\n'
+        else:
+            MSG = MSG + port + ' : Not Found my.cnf!\n'
+        
 def clean():
-	global MAIL_MSG
-	MAIL_MSG = MAIL_MSG + '<h1>清理过期备份</h1>'
-	for f in Path(BACKUP_DATADIR).glob('**/*.tar.gz'):
-		if (f.stat().st_mtime + CLEAN_DAY * 24 * 3600) < time.time():
-			MAIL_MSG = MAIL_MSG + '<p>Clean %s </p>' % str(f)
-			f.unlink()
+    global MSG
+    MSG = MSG + '清理过期备份\n'
+    for f in Path(BACKUP_DATADIR).glob('**/*.tar.gz'):
+        if (f.stat().st_mtime + CLEAN_DAY * 24 * 3600) < time.time():
+            MSG = MSG + 'Clean {} \n'.format(str(f))
+            f.unlink()
 
 def email():
-	global MAIL_MSG
-	sender = 'xxxxx'
-	pwd = 'xxxxxxxxx'
-	receivers = ['xxxxx@qq.com']
-	message = MIMEText(MAIL_MSG, 'html', 'utf-8')
-	message['From'] = Header('send@wanjizhijia.com', 'utf-8')
-	message['To'] =  Header('Bice.', 'utf-8')
-	message['Subject'] = Header('数据库备份报告', 'utf-8')
+    global MSG
+    sender = 'xxxxx'
+    pwd = 'xxxxxxxxx'
+    receivers = ['xxxxx@qq.com']
+    message = MIMEText(MSG, 'html', 'utf-8')
+    message['From'] = Header('send@xxxxxx.com', 'utf-8')
+    message['To'] =  Header('tony', 'utf-8')
+    message['Subject'] = Header('数据库备份报告', 'utf-8')
 
-	try:
-		smtpobj = smtplib.SMTP('smtp.exmail.qq.com')
-		smtpobj.login(sender, pwd)
-		smtpobj.sendmail(sender, receivers, message.as_string())
-		print('邮件发送成功')
-	except smtplib.SMTPException:
-		print('Error: 无法发送邮件')
-	
+    try:
+        smtpobj = smtplib.SMTP('smtp.exmail.qq.com')
+        smtpobj.login(sender, pwd)
+        smtpobj.sendmail(sender, receivers, message.as_string())
+        print('邮件发送成功')
+    except smtplib.SMTPException:
+        print('Error: 无法发送邮件')
+    
+def slack():
+    '''send backup messages to slack'''
+    global MSG
+    url='https://hooks.slack.com/services/TaaK9CD5/B9XF61X4J/oICgcpCBccscPAuRPGl4xGNQ'
+    username='backups'
+    payload={ 'payload': { 'channel': '#backups', 'username': username, 'text': MSG, 'icon_emoji': ':smile:' } }
+    req=urllib.request.Request(url, method='POST', data=urllib.parse.urlencode(payload).encode('utf-8'))
+    try:
+        with urllib.request.urlopen(req) as r:
+           print(r.getcode())
+    except urllib.error.URLError as e:
+           print(e)
+
 if __name__=='__main__':
-	backup()
-	clean()
-	email()
+    backup()
+    clean()
+    slack()       
+#   email()
 
